f9a0ea76f055f475a7a8e6d469635b71bfb77278
julietsanchez <112452557+julietsanchez@users.noreply.github.com>
2025-10-29 22:36:14 -0300

    Quitar fondo amarillo de info y cambiar iconos a cok-orange

--- app/(site)/servicios/page.tsx
+++ app/(site)/servicios/page.tsx
@@ -248,12 +248,12 @@ export default function ServiciosPage() {
               </div>
               
               {horneadaSection?.detalles?.informacionImportante && (
-                <div className="mt-6 p-4 bg-yellow-50 border border-yellow-200 rounded-lg">
+                <div className="mt-6">
                   <ul className="space-y-2">
                     {horneadaSection.detalles.informacionImportante.map((info, index) => (
                       <li key={index} className="flex items-start text-sm">
-                        <Info className="text-yellow-600 mr-3 mt-0.5 flex-shrink-0" size={16} />
-                        <span className="text-yellow-800">{info}</span>
+                        <Info className="text-cok-orange mr-3 mt-0.5 flex-shrink-0" size={16} />
+                        <span className="text-gray-600">{info}</span>
                       </li>
                     ))}
                   </ul>
